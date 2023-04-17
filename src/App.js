044ad8947c7d617ea43fa21825bf9641a926b9fe
radwa <radwa2484@gmail.com>
2023-04-17 10:10:14 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoListFC from "./components/Todo List FunctionalComponent/TodoListFC";
+const App = () => {
+  return (
+    <React.Fragment>
+      <TodoListFC />
+    </React.Fragment>
+  );
+};
+
+export default App;
